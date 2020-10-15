e06e79c8d3916334454d05ff19fb9091f2e7b9f4
Professor <Professor@WIN-MP3JADQD58A.local>
2020-10-15 12:22:16 +0100

    Update rc_at6_e.php

---
+++ rc_at6_e.php
@@ -0,0 +1,46 @@
+<?php
+
+//exercicio 1 soma de 2 numeros
+	$a=23;
+	$b=45;
+	$c=$a+$b;
+	echo "A soma de " .$a. " e " .$b. " é " .$c;
+
+	echo "<br>";
+	echo "<br>";
+
+//exercicio 2 media de 3 notas
+
+	$nota1=17;
+	$nota2=20;
+	$nota3=13;
+	$media=($nota1+$nota2+$nota3)/3;
+	echo "A média das tres notas: " .$nota1. ", " .$nota2. " e " .$nota3. " é " .$media;
+
+	echo "<br>";
+	echo "<br>";
+
+//exercicio 3 quociente e resto da divisao de 2 variaveis
+
+	$x=99;
+	$y=24;
+	$quociente=$x * $y;
+	$resto=$x % $y;
+	echo "O quociente entre " .$x. " e " .$y. " é " .$quociente;
+	echo "<br>";
+	echo "O resto da divisão entre " .$x. " e " .$y. " é " .$resto;
+
+	echo "<br>";
+	echo "<br>";
+
+//exercicio 4
+
+	$nota1=17;
+	$nota2=20;
+	$nota3=13;
+	$media=($nota1+$nota2+$nota3)/3;
+	echo "A média das tres notas: " .$nota1. ", " .$nota2. " e " .$nota3. " é " .$media;
+
+	echo "<br>";
+	echo "<br>";
+?>
